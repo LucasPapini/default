54a1f6defcfb0d329e4d5eb44bd9ad6d930197f3
LucasPapini <papine12345@hotmail.com>
2024-12-18 11:36:15 -0300

    Criando model que representa o usuario no banco, ou melhor, criando nossa primeira entidade.

---
+++ src/main/java/com/lucaspapini/defaultapi/model/User.java
@@ -0,0 +1,149 @@
+package com.lucaspapini.defaultapi.model;
+
+import java.security.Timestamp;
+
+public class User {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+  private Integer role;
+  private Integer isActive;
+  private Timestamp lastLogin;
+
+  public User() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Integer getRole() {
+    return role;
+  }
+
+  public void setRole(Integer role) {
+    this.role = role;
+  }
+
+  public Integer getIsActive() {
+    return isActive;
+  }
+
+  public void setIsActive(Integer isActive) {
+    this.isActive = isActive;
+  }
+
+  public Timestamp getLastLogin() {
+    return lastLogin;
+  }
+
+  public void setLastLogin(Timestamp lastLogin) {
+    this.lastLogin = lastLogin;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+    result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
+    result = prime * result + ((password == null) ? 0 : password.hashCode());
+    result = prime * result + ((role == null) ? 0 : role.hashCode());
+    result = prime * result + ((isActive == null) ? 0 : isActive.hashCode());
+    result = prime * result + ((lastLogin == null) ? 0 : lastLogin.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (firstName == null) {
+      if (other.firstName != null)
+        return false;
+    } else if (!firstName.equals(other.firstName))
+      return false;
+    if (lastName == null) {
+      if (other.lastName != null)
+        return false;
+    } else if (!lastName.equals(other.lastName))
+      return false;
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    } else if (!email.equals(other.email))
+      return false;
+    if (password == null) {
+      if (other.password != null)
+        return false;
+    } else if (!password.equals(other.password))
+      return false;
+    if (role == null) {
+      if (other.role != null)
+        return false;
+    } else if (!role.equals(other.role))
+      return false;
+    if (isActive == null) {
+      if (other.isActive != null)
+        return false;
+    } else if (!isActive.equals(other.isActive))
+      return false;
+    if (lastLogin == null) {
+      if (other.lastLogin != null)
+        return false;
+    } else if (!lastLogin.equals(other.lastLogin))
+      return false;
+    return true;
+  }
+
+}
