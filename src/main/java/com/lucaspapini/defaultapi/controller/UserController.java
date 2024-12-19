972ec4de892c75b8f439cf9b913526d5f86cbf00
LucasPapini <papine12345@hotmail.com>
2024-12-19 14:47:33 -0300

    Adicionei 2 novas variáveis no código uma que representa uma data sm String e outra que pega a data em string e converte para o tipo LocalDateTime. Foi adicionando novo endpoint para a criação de usuário.

--- src/main/java/com/lucaspapini/defaultapi/controller/UserController.java
+++ src/main/java/com/lucaspapini/defaultapi/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.lucaspapini.defaultapi.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.helpers.Reporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lucaspapini.defaultapi.model.User;
 import com.lucaspapini.defaultapi.services.UserServices;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/api/v1/user")
@@ -22,8 +26,14 @@ public class UserController {
 
   @GetMapping(value = "/fakeUser", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<User> fakeUserTesting() {
+    // String no formato ISO 8601
+    String lastLoginString = "2024-12-19T14:30:00";
+
+    // Converter para LocalDateTime
+    LocalDateTime lastLogin = LocalDateTime.parse(lastLoginString);
+
     User user = new User(1L, "Lucas", "Papin", "lucas.papini@gmail.com.br", "123", 1,
-        1);
+        1, lastLogin);
     return ResponseEntity.ok().body(user);
   }
 
@@ -37,6 +47,12 @@ public class UserController {
   public ResponseEntity<User> findById(@PathVariable Long id) {
     User obj = services.findById(id);
     return ResponseEntity.ok().body(obj);
+  }
+
+  @PostMapping
+  public ResponseEntity<User> create(@RequestBody User params) {
+    User newUser = services.insert(params);
+    return ResponseEntity.ok().body(newUser);
   }
 
 }
