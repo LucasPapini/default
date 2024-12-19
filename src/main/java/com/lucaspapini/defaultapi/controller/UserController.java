94a07a8e235de8a01751cbca42a20a14634a768f
LucasPapini <papine12345@hotmail.com>
2024-12-19 11:05:28 -0300

    Foi add nesta controller a injeção da class Servies (instanciando a UserService) com essa injeção no podemos ter acesso aos métodos modificadores que se encontra na sercive de User (UserService). Foi feito também a modificação no endpoint de consulta da api para consultar uma instaciação da entidade user para debugs. Criado também 2 novos endpoints para consultar todos os usuários e consulta por id.

--- src/main/java/com/lucaspapini/defaultapi/controller/UserController.java
+++ src/main/java/com/lucaspapini/defaultapi/controller/UserController.java
@@ -1,22 +1,42 @@
 package com.lucaspapini.defaultapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucaspapini.defaultapi.model.User;
+import com.lucaspapini.defaultapi.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/api/v1/user")
 public class UserController {
 
-  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<User> fakeUserTesting() {
+  @Autowired
+  UserServices services;
 
+  @GetMapping(value = "/fakeUser", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<User> fakeUserTesting() {
     User user = new User(1L, "Lucas", "Papin", "lucas.papini@gmail.com.br", "123", 1,
         1);
     return ResponseEntity.ok().body(user);
   }
+
+  @GetMapping
+  public ResponseEntity<List<User>> findAll() {
+    List<User> users = services.findAll();
+    return ResponseEntity.ok().body(users);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<User> findById(@PathVariable Long id) {
+    User obj = services.findById(id);
+    return ResponseEntity.ok().body(obj);
+  }
+
 }
