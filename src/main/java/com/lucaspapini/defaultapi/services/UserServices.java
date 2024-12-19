2e48f19623a76778731d91361a0f5fb4ae524552
LucasPapini <papine12345@hotmail.com>
2024-12-19 14:51:01 -0300

    Em serviços eu adicionei o Logger para fins de debus visual no console, e também criei um novo método para a criação de usuário, método chamado insert...

--- src/main/java/com/lucaspapini/defaultapi/services/UserServices.java
+++ src/main/java/com/lucaspapini/defaultapi/services/UserServices.java
@@ -2,6 +2,7 @@ package com.lucaspapini.defaultapi.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ import com.lucaspapini.defaultapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserServices {
+  private Logger logger = Logger.getLogger(UserServices.class.getName());
+
   @Autowired
   private UserRepository repository;
 
@@ -22,6 +25,11 @@ public class UserServices {
   public User findById(Long id) {
     Optional<User> obj = repository.findById(id);
     return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+  }
+
+  public User insert(User params) {
+    logger.info("Creating a new user...");
+    return repository.save(params);
   }
 
 }
